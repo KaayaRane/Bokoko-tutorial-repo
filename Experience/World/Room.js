e4a16bb96c02afad2ce8047608bcd351ae0e0ccc
KaayaRane <63211165+KaayaRane@users.noreply.github.com>
2023-05-03 12:34:51 -0500

    created video texture and could add it to scene

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -8,6 +8,7 @@ export default class Room{
         this.resources = this.experience.resources;
         this.room = this.resources.items.room;
         this.actualRoom = this.room.scene;
+        console.log(this.actualRoom);
         
         this.setModel();
 
@@ -25,9 +26,18 @@ export default class Room{
                     child.receiveShadow = true;
 
                 })
+                //console.log("found children")
             }
 
-        })
+            if(child.name === "Cube005"){
+                child.material = new THREE.MeshBasicMaterial({
+                    map: this.resources.items.screen,
+                });
+                console.log("found cube 005")
+                
+            }
+
+        });
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.225, 0.225, 0.225);
         //this.actualRoom.translateY(0.8);
