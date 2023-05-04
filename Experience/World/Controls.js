d0947d34c932693666186834f0daff2575237de3
KaayaRane <63211165+KaayaRane@users.noreply.github.com>
2023-05-04 17:40:55 -0500

    added some more html and css, and added scroll trigger animations

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,14 +1,19 @@
 import * as THREE from 'three';
 import Experience from "../Experience.js";
 import GSAP from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js"
 
 export default class Controls{
     constructor(){
         this.experience = new Experience();
         this.scene = this.experience.scene;
         this.resources = this.experience.resources;
+        this.sizes = this.experience.sizes;
         this.time = this.experience.time;
         this.camera = this.experience.camera;
+        this.room = this.experience.world.room.actualRoom;
+        GSAP.registerPlugin(ScrollTrigger);
+        this.setPath();
         
         // Vectors and progress utilized for basic controls
         // this.progress = 0;
@@ -24,6 +29,21 @@ export default class Controls{
         // this.setPath();
         // this.onWheel();
 
+    }
+
+    setPath(){
+        this.timeline = new GSAP.timeline();
+        this.timeline.to(this.room.position, {
+            x: ()=>{return this.sizes.width * .0018},
+            scrollTrigger: {
+                trigger: ".first-move-section-margin",
+                markers: true,
+                start: "top top",
+                end: "bottom bottom",
+                scrub: 0.5,
+                invalidateOnRefresh: true,
+            }
+        });
     }
 
 
